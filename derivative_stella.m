57904a0140d3a2d2d1d2150dd52e7a38ac0a00f3
stellasap <styliani_sapantzi@brown.edu>
2023-03-16 20:47:25 -0400

    Stella figured out how to display and open up derivative, we came up with a plan for our final code

--- derivative_stella.m
+++ derivative_stella.m
@@ -4,11 +4,18 @@
 d = abfload('22d11014.abf','start',0,'stop','e');
 size(d)
 close all
-lowerl = 20000
-upperl = 25000
-plot(lowerl:upperl, d(lowerl:upperl,3,1))
+lowerl = 30000
+upperl = 40000
+plot(lowerl:upperl, d(lowerl:upperl,3,1));
 xlabel('Time(ms)')
 ylabel('Membrane Potential (mV)')
 
-dy=diff(d(lowerl:upperl,3,1))./diff(lowerl:upperl)
-plot(lowerl:upperl, dy)
+%derivative of Vm (change in Vm over change of time), looking for dVm/dt==0
+%the derivative plot is called a phase plot, and it is useful data for
+%action potentials so we should keep it and make it pretty
+dy=diff(d(lowerl:upperl,3,1))./diff(lowerl:upperl);
+
+size(dy)
+plot(lowerl:upperl,dy(lowerl:upperl))
+
+%this created a phase plot for the action potentials 
