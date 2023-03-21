873a5f6f2e56e04d1ed8d6088216f408d4d1a365
camille-aquino <111753830+camille-aquino@users.noreply.github.com>
2023-03-21 16:03:02 -0400

    working on counting derivative

--- count_derivatives_camille.m
+++ count_derivatives_camille.m
@@ -1,31 +1,36 @@
-function [numzero] = count_derivatives_camille(d, lowerl, upperl, sweep, width)
+function [success] = count_derivatives_camille(d, lowerl, upperl, sweep, width)
 
-% width is different for different protocols/frequencies (30 Hz, 60 Hz,
-% etc.)
-
-% counting the number of derivatives that equal 0
+% counting the number of derivatives that are close to 0
 
 % Step 1: form derivative -- diff(y) - diff(x)
 
-% making derivative of every single vm measurement is too much! need to
-% find width of each AP so I can go up by that amount only
-
 dy = diff(d(lowerl:width:upperl,3,sweep)) ./ diff(lowerl:width:upperl);
 
-numzero = 0;
+% detect when the derivative is very different from 0 (difference > ?), then
+% start counting APs
 
-for i = lowerl:upperl
-    if (dy >= -0.01 && dy >= 0.01)
-        numzero = numzero + 1;
+start_loc = [0 0 0];
+
+while(true)
+    if (dy >= 2)
+start_loc = 
+        break
     end
 end
 
+    numzero = [];
+
+numzero = dy(dy > -0.01 & dy < 0.01)
+        
+success = length(numzero) > 
+
 end
+
 
 %Check:      
 %d = abfload('22d11014.abf','start',0,'stop','e');
 %size(d)
 %lowerl = 1;
-%upperl = 60000;
+%upperl = 10000;
 %sweep = 1;
 % width = 2
