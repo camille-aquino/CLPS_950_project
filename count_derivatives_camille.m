d9ad91c69cd0a1a42049cf5cd8b4a64b1d7793de
camille-aquino <111753830+camille-aquino@users.noreply.github.com>
2023-03-22 23:35:32 -0400

    editing to make it a count ap function
    parsing through each AP (assuming width of 4000 ms??, will have to double check with stella) and counting number of times the derivative = 0. if the number of times it = 0 is 4 or more, that means it was a successful AP. I used it as a threshold though, not exactly zero but less than 0.01
    
    time: 1 hour

--- count_derivatives_camille.m
+++ count_derivatives_camille.m
@@ -1,4 +1,4 @@
-function [success] = count_derivatives_camille(d, lowerl, upperl, sweep, width)
+function [count_ap] = count_derivatives_camille(d, lowerl, upperl, sweep, width)
 
 % counting the number of derivatives that are close to 0
 
@@ -10,22 +10,36 @@ dy = diff(d(lowerl:upperl,3,sweep)) ./ diff(lowerl:upperl);
 % start counting APs
 
 
-start_loc = find(dy>0.01, 1); % finds first place where dy is much greater than 0 (i.e. 0.5)
+start_loc = find(abs(dy) > 0.1, 1); % finds first place where dy is much greater than 0 (i.e. 0.5)
 
-dy = dy(start_loc:end);
+dy = dy(start_loc:end); % but is this in the right dimension??
 
 numzero = [];
 
-numzero = sum(dy == 0);
+count_ap = 0;
+
+for i = start_loc:4000:upperl % assuming 4000 is the width of each ap
+    dy_bit = dy(i:i+400)
+    numzero = sum(abs(dy_bit) <= 0.01); % if close to 0
+    if(numzero >= 4) % successful AP if at least 4 0s
+        count_ap = count_ap+1;
+    end
+end
+
+count_ap
 
 end
 
+% finished finding total num zeros
+% now to count derivatives must parse through every 4*10^4 inputs??? and count
+% number of zeros for that?
+% how to parse through this 
 
 
 %Check:      
-d = abfload('22d11014.abf','start',0,'stop','e');
-size(d)
-lowerl = 1;
-upperl = 10000;
-sweep = 1;
- width = 0.08
+%d = abfload('22d11014.abf','start',0,'stop','e');
+%size(d)
+%lowerl = 1;
+%upperl = 30000;
+%sweep = 1;
+%width = 0.08
